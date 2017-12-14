9d64f8036abfa40700f3d57c0c5f480f72fd3adb
Marc Farra <marcfarra@gmail.com>
2017-12-14 00:31:47 -0500

    Add pooling

--- tilepie/__init__.py
+++ tilepie/__init__.py
@@ -2,6 +2,7 @@ import landez
 import logging
 import gzip
 import StringIO
+import multiprocessing as mp
 
 def uncompress(compressed):
   fileobj = StringIO.StringIO(compressed)
@@ -12,6 +13,8 @@ def uncompress(compressed):
   return uncompressed
 
 def tilereduce (options, mapper, callback, done):
+  pool = mp.Pool()
+
   tm = landez.TilesManager(mbtiles_file=options.get('source'))
   zoom = options.get('zoom')
   tiles = tm.tileslist(bbox=options.get('bbox'), zoomlevels=[zoom])
@@ -19,8 +22,9 @@ def tilereduce (options, mapper, callback, done):
   for tile in tiles:
     try: 
       tilecontent = uncompress(tm.tile(tile))
-      mapped = mapper(tile[1], tile[2], tile[0], tilecontent)
-      callback(mapped)
+      pool.apply_async(mapper, args=(tile[1], tile[2], tile[0], tilecontent), callback = callback)
     except landez.ExtractionError:
       pass
+  pool.close()
+  pool.join()
   done()
